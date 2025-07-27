c300ff97b0f6ca0af70f65c6a5913d810d043bc6
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-07-27 09:53:21 +0500

    language

--- src/locales/eng.ts
+++ src/locales/eng.ts
@@ -1,44 +1,70 @@
 export const eng = {
-    navbar:{
-        home:'Home',
-        products:'Products',
-        about:'About Us',
-        contact:"Contact",
+  navbar: {
+    home: "Home",
+    products: "Products",
+    about: "About Us",
+    contact: "Contact",
+  },
+  buttons: {
+    order: "Ordering",
+    remind: "Place an order",
+    send: "Send",
+  },
+  contact_comp: {
+    title: "Place an order!",
+    desc: "Order now to experience the unique taste in every sip!",
+    form: {
+      userName: "Your name",
+      phoneNumber: "Your phone number",
     },
-    buttons:{
-        order:"Ordering",
-        remind:"Place an order",
-        send:"Send"
+  },
+  homePage: {
+    homeAbout: {
+      title: "About Us",
+      desc: `Chere Water pure spring water has been operating for its customers since 2023. One of the highest goals of our company is to produce and supply mineral-rich water products in accordance with quality standards. Our filtration technologies occupy high positions in Uzbekistan. Our products are adapted for your office, family and training and combine bright flavors`,
     },
-    contact_comp:{
-        title:'Place an order!',
-        desc:'Order now to experience the unique taste in every sip!',
-        form:{
-            userName:'Your name',
-            phoneNumber:'Your phone number'
-        }
+    homeCargo: {
+      title: "Free Shipping",
+      desc: `Customers who regularly purchase 20 20-liter bottles of water each month will receive a free cooler.`,
     },
-    homePage:{
-        homeAbout:{
-            title:'About Us',
-            desc:`Chere Water pure spring water has been operating for its customers since 2023. One of the highest goals of our company is to produce and supply mineral-rich water products in accordance with quality standards. Our filtration technologies occupy high positions in Uzbekistan. Our products are adapted for your office, family and training and combine bright flavors`
-        },
-        homeCargo:{
-            title:'Free Shipping',
-            desc:`Customers who regularly purchase 20 20-liter bottles of water each month will receive a free cooler.`
-        },
-        homeWhyUs:{
-            title:'Why Chere?',
-            title1:'Pure spring water',
-            desc1:`Chere water is a spring water rich in minerals and energy that will energize your body!`,
-            title2:'Special water filter',
-            desc2:`The product is 100% filtered and arrives at your home completely free of toxins!`,
-            title3:'Health Choice',
-            desc3:`This spring water is an ideal choice for internal cleansing and balancing the body!`,
-        },
-        homeSer:{
-            title:'Chere water quality certificate',
-            desc:`Chere water meets all standards. It combines minerals. The certificate confirms this for the confidence of its customers.`
-        },
+    homeWhyUs: {
+      title: "Why Chere?",
+      title1: "Pure spring water",
+      desc1: `Chere water is a spring water rich in minerals and energy that will energize your body!`,
+      title2: "Special water filter",
+      desc2: `The product is 100% filtered and arrives at your home completely free of toxins!`,
+      title3: "Health Choice",
+      desc3: `This spring water is an ideal choice for internal cleansing and balancing the body!`,
     },
+    homeSer: {
+      title: "Chere water quality certificate",
+      desc: `Chere water meets all standards. It combines minerals. The certificate confirms this for the confidence of its customers.`,
+    },
+  },
+  productsPage: {
+    titelePart: {
+      title: "Our Products",
+      desc: `A pleasant discovery and beneficial minerals in every sip. Try it, be refreshed and healthy!`,
+    },
+    productsTitle: {
+      title1: "On the go, at home and at work. Chere Water",
+      title2: "With You Everywhere!",
+    },
+  },
+  aboutPage: {
+    title: "About Us",
+    desc: `Our main goal is to package and deliver high-quality and environmentally friendly beverages.`,
+    videoText: `Watch the video to see how our company has grown and how we manage our water production process.
+We are built on our values and your trust!`,
+  },
+  contactPage: {
+    title: "Leave a message",
+    desc: `Leave your information and our operator will contact you immediately.`,
+  },
+  suvContact: {
+    location:
+      "Address: Tashkent city, Uchtepa district, Bogishamol avenue, house 3",
+    phone: "Contact us:",
+    time: "Application time:",
+  },
 };
--- src/locales/uz.ts
+++ src/locales/uz.ts
@@ -1,44 +1,70 @@
 export const uz = {
-    navbar:{
-        home:'Bosh sahifa',
-        products:'Mahsulotlarimiz',
-        about:'Biz haqimizda',
-        contact:"Bog'lanish",
+  navbar: {
+    home: "Bosh sahifa",
+    products: "Mahsulotlarimiz",
+    about: "Biz haqimizda",
+    contact: "Bog'lanish",
+  },
+  buttons: {
+    order: "Buyrtma berish",
+    remind: "Buyrtma qoldirish",
+    send: "Jo'natish",
+  },
+  contact_comp: {
+    title: "Buyurtma Qoldiring !",
+    desc: "Har bir qultumda betakror tamni sinab ko‘rish uchun hoziroq buyutma bering!",
+    form: {
+      userName: "Ismingiz",
+      phoneNumber: "Telefon raqamingiz",
     },
-    buttons:{
-        order:"Buyrtma berish",
-        remind:"Buyrtma qoldirish",
-        send:"Jo'natish"
+  },
+  homePage: {
+    homeAbout: {
+      title: "Biz Haqimizda",
+      desc: `Chere Water toza buloq suvlari  2023-yildan buyon o'z mijozlari uchun faoliyat yuritib kelmoqda. Bizning kompaniyaning oliy maqsadlaridan biri minerallarga boy suv mahsulotlarini sifat standartlariga mos ishlab chiqarish va yetkazib berishdir. Bizning filtrlash texnalogiyalarimiz O'zbekistonda yuqori o'rinlarni egallaydi. Mahsulotimiz sizning ofisingiz, oilangiz va mashg'ulotlaringiz uchun moslashtirilgan hamda o'zida yorqin ta'mlarni uyg'unlashtirgan`,
     },
-    contact_comp:{
-        title:'Buyurtma Qoldiring !',
-        desc:'Har bir qultumda betakror tamni sinab ko‘rish uchun hoziroq buyutma bering!',
-        form:{
-            userName:'Ismingiz',
-            phoneNumber:'Telefon raqamingiz'
-        }
+    homeCargo: {
+      title: "Bepul yetkazib Berish",
+      desc: `Har oy 20 dona 20 l hajmdagi suvdan muntazam ravishda xarid qilgan mijozlarga kuller apparati bepul taqdim etiladi.`,
     },
-    homePage:{
-        homeAbout:{
-            title:'Biz Haqimizda',
-            desc:`Chere Water toza buloq suvlari  2023-yildan buyon o'z mijozlari uchun faoliyat yuritib kelmoqda. Bizning kompaniyaning oliy maqsadlaridan biri minerallarga boy suv mahsulotlarini sifat standartlariga mos ishlab chiqarish va yetkazib berishdir. Bizning filtrlash texnalogiyalarimiz O'zbekistonda yuqori o'rinlarni egallaydi. Mahsulotimiz sizning ofisingiz, oilangiz va mashg'ulotlaringiz uchun moslashtirilgan hamda o'zida yorqin ta'mlarni uyg'unlashtirgan`
-        },
-        homeCargo:{
-            title:'Bepul yetkazib Berish',
-            desc:`Har oy 20 dona 20 l hajmdagi suvdan muntazam ravishda xarid qilgan mijozlarga kuller apparati bepul taqdim etiladi.`
-        },
-        homeWhyUs:{
-            title:'Nega Chere ?',
-            title1:'Toza buloq suvi',
-            desc1:`Chere water mineral va energiyaga to‘yingan buloq suvi bo‘lib, tanagizga quvvat baxsh etadi!`,
-            title2:'Maxsus suv filti',
-            desc2:`Mahsulot 100% fitrlangan bo‘lib, toksinlardan to‘liq tozalangan holda xonadoningizga yetib boradi!`,
-            title3:'sog‘liq tanlovi',
-            desc3:`Ushbu buloq suvi organizmni ichki tozalash va balansda ushlab turish uchun ideal tanlovdir!`,
-        },
-        homeSer:{
-            title:'Chere water sifat sertifikati',
-            desc:`Chere water barcha standartlarga javob beradi. Minerallarni o'zida uyg'unlashtiradi. Mijozlari ishonchi uchun sertifikat buni tasdiqlaydi.`
-        },
+    homeWhyUs: {
+      title: "Nega Chere ?",
+      title1: "Toza buloq suvi",
+      desc1: `Chere water mineral va energiyaga to‘yingan buloq suvi bo‘lib, tanagizga quvvat baxsh etadi!`,
+      title2: "Maxsus suv filti",
+      desc2: `Mahsulot 100% fitrlangan bo‘lib, toksinlardan to‘liq tozalangan holda xonadoningizga yetib boradi!`,
+      title3: "sog‘liq tanlovi",
+      desc3: `Ushbu buloq suvi organizmni ichki tozalash va balansda ushlab turish uchun ideal tanlovdir!`,
     },
+    homeSer: {
+      title: "Chere water sifat sertifikati",
+      desc: `Chere water barcha standartlarga javob beradi. Minerallarni o'zida uyg'unlashtiradi. Mijozlari ishonchi uchun sertifikat buni tasdiqlaydi.`,
+    },
+  },
+  productsPage: {
+    titelePart: {
+      title: "Bizning Mahsulotlar",
+      desc: `Har bir qultumda yoqimli kashfiyot va foydali minerallar. Ta'tib ko'ring, tetik va sog'lom bo'ling!`,
+    },
+    productsTitle: {
+      title1: "Yo‘lda, Uyda va Ishda. Chere Water",
+      title2: "Har Joyda Siz Bilan Birga!",
+    },
+  },
+  aboutPage: {
+    title: "Biz haqimizda",
+    desc: `Bizning asosiy maqsadimiz yuqori sifatli va ekologik toza ichimliklarni qadoqlash va yetkazib berishdan iborat.`,
+    videoText: `Videoda kompaniyamizning rivojlanish yo‘lini va suv ishlab chiqarish jarayonini qanday boshqarayotganimizni ko‘ring. 
+Biz qadriyatlarimiz va sizning ishonchingizga suyanamiz!`,
+  },
+  contactPage: {
+    title: "Ma’lumot qoldiring",
+    desc: `Ma’lumotlaringizni qoldiring va operatorimiz sizga darhol aloqaga chiqadi.`,
+  },
+  suvContact: {
+    location:
+      "Manzil: Toshkent shahar, Uchtepa tumaniBog‘ishamol koz-‘chasi, 3-uy",
+    phone: "Biz bilan bog‘laning:",
+    time: "Murojaat vaqti:",
+  },
 };
