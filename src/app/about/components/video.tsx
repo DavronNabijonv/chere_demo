e5587998bac2e7ae08e9b952efd64935d2271f60
Davron Chetin <nabijonovdavronbek619@gmail.com>
2025-07-28 12:05:48 +0500

    video part

--- src/app/about/components/video.tsx
+++ src/app/about/components/video.tsx
@@ -4,14 +4,19 @@ export default function Video() {
   const t = useTranslations();
   return (
     <div className="mx-auto flex w-full max-w-[1200px] flex-col items-center justify-center gap-10 py-10">
-      <p className="mx-auto w-full max-w-[800px] bg-gradient-to-r from-[#1A3B8D] via-[#000000] to-[#CCA55D] bg-clip-text text-center text-[30px] font-[500] text-transparent">
-        {t.aboutPage.videoText}
-      </p>
-      <video
-        src={"https://youtu.be/x6GxcslSMFc?si=rMqF7HF7FvgZoDAV"}
-        controls
-        className="h-full w-full cursor-pointer rounded-lg object-cover shadow-md"
-      />
-    </div>
+    <p className="mx-auto w-full max-w-[800px] bg-gradient-to-r from-[#1A3B8D] via-[#000000] to-[#CCA55D] bg-clip-text text-center text-[30px] font-[500] text-transparent">
+      {t.aboutPage.videoText}
+    </p>
+    <iframe
+      width="100%"
+      height="500"
+      src="https://www.youtube.com/embed/cWD4YD9BMNY"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      className="rounded-lg shadow-md"
+    ></iframe>
+  </div>
   );
 }
